1a792b7c93a7087efb778080451607d8d770060a
yuna096740 <yyuusseeii1111@gmail.com>
2023-11-18 11:42:03 +0900

    [add] twitter demo app

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'yuna @ flutter lesson',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                child: Image.asset(
+                  'assets/images/avotor.png',
+                ),
+              ),
+              SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text('yuna @ flutter lesson'),
+                      SizedBox(width: 8),
+                      Text('2023/11/18'),
+                    ],
+                  ),
+                  SizedBox(height: 4),
+                  Text('練習中です'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
