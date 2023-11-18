5eb1cf927aad47cdf472fbd0a676c95e9cef7df7
yuna096740 <yyuusseeii1111@gmail.com>
2023-11-18 11:54:47 +0900

    [add] tweet wedget

--- lib/main.dart
+++ lib/main.dart
@@ -22,34 +22,49 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
+        body: TweetTitle(),
+      ),
+    );
+  }
+}
+
+class TweetTitle extends StatelessWidget {
+  const TweetTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CircleAvatar(
+            child: Image.asset(
+              'assets/images/avotor.png',
+            ),
+          ),
+          SizedBox(width: 8),
+          Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CircleAvatar(
-                child: Image.asset(
-                  'assets/images/avotor.png',
-                ),
-              ),
-              SizedBox(width: 8),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      Text('yuna @ flutter lesson'),
-                      SizedBox(width: 8),
-                      Text('2023/11/18'),
-                    ],
-                  ),
-                  SizedBox(height: 4),
-                  Text('練習中です'),
+                  Text('yuna @ flutter lesson'),
+                  SizedBox(width: 8),
+                  Text('2023/11/18'),
                 ],
+              ),
+              SizedBox(height: 4),
+              Text('練習中です'),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_border),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
